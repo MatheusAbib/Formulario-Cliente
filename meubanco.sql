5fa85512ab42e43e7016f1cdfabc999771d2b29c
Matheus Abib <matheus.abib.ma@gmail.com>
2025-06-30 10:56:09 -0300

    Arquivo Sql Incluido

---
+++ meubanco.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.9
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tempo de Geração: 30/06/2025 às 15h55min
+-- Versão do Servidor: 5.5.20
+-- Versão do PHP: 5.3.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Banco de Dados: `meubanco`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL,
+  `endereco_entrega` varchar(255) DEFAULT NULL,
+  `descricao_endereco` varchar(255) DEFAULT NULL,
+  `codigo_seguranca` varchar(255) DEFAULT NULL,
+  `numero_cartao` varchar(255) DEFAULT NULL,
+  `data_alteracao` datetime DEFAULT NULL,
+  `data_cadastro` datetime DEFAULT NULL,
+  `data_validade` varchar(255) DEFAULT NULL,
+  `endereco_id` bigint(20) DEFAULT NULL,
+  `bairro` varchar(255) DEFAULT NULL,
+  `cep` varchar(255) DEFAULT NULL,
+  `cidade` varchar(255) DEFAULT NULL,
+  `estado` varchar(255) DEFAULT NULL,
+  `logradouro` varchar(255) DEFAULT NULL,
+  `numero` varchar(255) DEFAULT NULL,
+  `pais` varchar(255) DEFAULT NULL,
+  `tipo_residencia` varchar(255) DEFAULT NULL,
+  `nome_cartao` varchar(255) DEFAULT NULL,
+  `bandeira` varchar(255) DEFAULT NULL,
+  `genero` varchar(255) DEFAULT NULL,
+  `data_nascimento` varchar(255) DEFAULT NULL,
+  `cpf` varchar(255) DEFAULT NULL,
+  `telefone` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK7v21uy9djyl7hh9464kkjsjg0` (`endereco_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT AUTO_INCREMENT=16 ;
+
+--
+-- Extraindo dados da tabela `cliente`
+--
+
+INSERT INTO `cliente` (`id`, `nome`, `email`, `senha`, `endereco_entrega`, `descricao_endereco`, `codigo_seguranca`, `numero_cartao`, `data_alteracao`, `data_cadastro`, `data_validade`, `endereco_id`, `bairro`, `cep`, `cidade`, `estado`, `logradouro`, `numero`, `pais`, `tipo_residencia`, `nome_cartao`, `bandeira`, `genero`, `data_nascimento`, `cpf`, `telefone`) VALUES
+(12, 'aaa', 'aaa@gmail.com', '12345Ma#', 'sssss', 'ssssss', '111', '1234567', NULL, '2025-06-30 10:06:59', NULL, NULL, 'aaaaaa', '11222', 'ssss', 'ssss', '1111', '1111', 'Brasil', 'aaaa', 'matheus', 'brasl', 'masculino', '2025-06-07', '422.113.848-38', '(11) 97507-2008'),
+(13, 'Matheus', 'matheus@gmail.com', '1234Mat#', 'AAAAA', 'AAAAAA', '123', '11111111111', NULL, '2025-06-30 10:16:47', NULL, NULL, 'AAA', '11111', 'AAA', 'AAAA', '1111', '111', 'Brasil', 'AA', 'Matheus', 'Brasll', 'masculino', '2025-06-12', '42211384838', '1234567'),
+(14, 'matheus', 'matheus@gmail.com', '123Mat#', 'aaaa', 'aaaaa', '111', '1234356', NULL, '2025-06-30 10:36:58', NULL, NULL, 'aaaa', '1111', 'aaaa', 'aaaa', '12223', '1111', 'Brasil', 'casa', 'Mathes', 'Santander', 'feminino', '2025-06-12', '42211384838', '11975072008'),
+(15, 'matheus', 'matheus@gmail.com', 'Ma20092004#', 'aaaa', 'aaaaa', '111', '1234356', NULL, '2025-06-30 10:49:34', NULL, NULL, 'aaaa', '1111', 'aaaa', 'aaaa', '12223', '1111', 'Brasil', 'casa', 'Mathes', 'Santander', 'feminino', '2025-06-12', '42211384838', '11975072008');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
